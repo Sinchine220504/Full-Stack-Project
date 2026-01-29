c533c8412ea81faca4cb3511471b867c0208ab1d
Sinchine Das <119046553+Sinchine220504@users.noreply.github.com>
2026-01-29 19:33:50 +0530

    Update project files and settings

--- backend-socialbooster-demo/socialbooster/settings.py
+++ backend-socialbooster-demo/socialbooster/settings.py
@@ -21,7 +21,11 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [
+    "full-stack-project-x6lm.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
